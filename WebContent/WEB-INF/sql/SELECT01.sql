75ef5fa79c2f26a3f763344b015fd466fdb9c9dc
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-27 15:46:15 +0900

    20210527 6교시 수업내용

--- WebContent/WEB-INF/sql/SELECT01.sql
+++ WebContent/WEB-INF/sql/SELECT01.sql
@@ -8,3 +8,13 @@ SELECT CustomerName FROM customers;
 */
 
 -- 한 줄 주석 (ctrl + /)로 주석 여부를 처리해줄 수 있다.
+
+-- 테이블 조회
+SELECT * FROM Categories; 
+SELECT * FROM Customers; -- 91
+SELECT * FROM Employees; -- 10
+SELECT * FROM OrderDetails; 
+SELECT * FROM Oreders;
+SELECT * FROM Products;
+SELECT * FROM Shippers;
+SELECT * FROM Suppliers;
---
+++ WebContent/WEB-INF/sql/SELECT02.sql
@@ -0,0 +1,59 @@
+-- WHERE : row를 조건으로 필터
+SELECT * FROM Customers WHERE Country="Mexico";
+
+SELECT * FROM Customers WHERE CustomerID= 1;
+
+SELECT * FROM Products WHERE Price = 18;
+
+SELECT * FROM Products WHERE Price >30;
+
+SELECT * FROM Products WHERE Price <30;
+
+SELECT * FROM Products WHERE Price <=30;
+
+SELECT * FROM Products WHERE Price >=30;
+
+SELECT * FROM Products WHERE Price <>30;
+
+SELECT * FROM Products WHERE Price !=30;
+
+SELECT * FROM Products WHERE Price BETWEEN 53 AND 55;
+
+-- like
+SELECT * FROM Customers WHERE City LIKE 'S%'; -- 대소문자 구분 안함
+-- 앞에 글자가 있다면
+
+SELECT * FROM Customers WHERE City LIKE '%g'; -- 뒤에 글자가 있다면
+
+SELECT * FROM Customers WHERE City like'%z%'; -- 중간에 z가 있다면alter
+
+SELECT * FROM Customers WHERE Country LIKE '%US%'; -- 여러 캐릭터 선언가능
+
+-- IN 
+SELECT * FROM Customers WHERE City IN ('paris', 'London');
+
+SELECT * FROM Customers WHERE City IN ('paris', 'London', 'Berlin');
+
+SELECT * FROM Customers WHERE City = "Berlin";
+
+SELECT * FROM Customers WHERE NOT City = "Berlin";
+
+SELECT * FROM Country WHERE NOT City LIKE "%on";
+
+SELECT * FROM Customers WHERE CustomerID = 32;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
