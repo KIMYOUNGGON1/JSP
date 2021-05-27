6e9c4e9ddd05af86f796c4dc1e9a6c6a52021413
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-27 16:31:02 +0900

    20210527 7교시 추가 내용

--- WebContent/WEB-INF/sql/SELECT03DISTINCT.sql
+++ WebContent/WEB-INF/sql/SELECT03DISTINCT.sql
@@ -8,3 +8,7 @@ SELECT COUNT(*) FROM Customers;
 SELECT COUNT(City) FROM Customers;
 
 SELECT COUNT(DISTINCT City) FROM Customers;
+
+SELECT DISTINCT Country FROM Customers;
+
+SELECT DISTINCT City, Country FROM Customers;
