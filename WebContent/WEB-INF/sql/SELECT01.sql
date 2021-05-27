340ccb736af17fe43e9c602707e27c6d3a7a8adc
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-27 14:47:30 +0900

    20210527 5교시 수업내용

---
+++ WebContent/WEB-INF/sql/SELECT01.sql
@@ -0,0 +1,10 @@
+SELECT CustomerName, City FROM Customers;
+SELECT * FROM Customers;
+SELECT customername FROM Customers;
+SELECT CustomerName FROM customers;
+
+/*
+ 여러줄 주석
+*/
+
+-- 한 줄 주석 (ctrl + /)로 주석 여부를 처리해줄 수 있다.
