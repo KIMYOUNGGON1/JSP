92e50656ba75e45c03b3ed03eefa042c3d0363cb
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-28 09:47:57 +0900

    20210528 1교시 수업내용 추가(import 주의 할 것)

--- src/ch14/JDBC01Servlet.java
+++ src/ch14/JDBC01Servlet.java
@@ -1,6 +1,7 @@
 package ch14;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.corba.se.pept.transport.Connection;
+
 
 /**
  * Servlet implementation class JDBC01Servlet
