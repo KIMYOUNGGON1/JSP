35cac1391f32987d98ce04b6b3d566e292cb4120
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-28 11:42:50 +0900

    20210528 3교시 수업내용

--- WebContent/WEB-INF/sql/SELECT04.sql
+++ WebContent/WEB-INF/sql/SELECT04.sql
@@ -4,3 +4,16 @@ WHERE CustomerID = 1;
 SELECT CustomerName, City
 FROM Customers
 WHERE CustomerID = 1;
+
+SELECT CustomerID, CustomerName, City
+FROM Customers
+WHERE CustomerID = 2;
+
+SELECT CustomerID
+FROM Customers;
+
+SELECT * 
+FROM Employees
+WHERE EmployeeID = 1;
+
+SELECT DISTINCT City FROM Customers;
