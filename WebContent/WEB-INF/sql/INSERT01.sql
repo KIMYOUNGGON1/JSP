3a10fa98e6ce9a576fa6d978132b436c28e1e56c
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-31 14:54:30 +0900

    20210531 추가 내용

---
+++ WebContent/WEB-INF/sql/INSERT01.sql
@@ -0,0 +1,45 @@
+SELECT * FROM Customers
+ORDER BY CustomerID DESC;
+
+INSERT INTO Customers
+(CustomerID, CustomerName,
+ ContactName, Address,
+ City, PostalCode, Country)
+VALUES
+(92, 'Kim', 'Lee', 'Gangnam', 'Seoul', '00000', 'Korea');
+
+INSERT INTO Customers
+(CustomerID, CustomerName,
+ ContactName, Address,
+ City, PostalCode, Country)
+VALUES
+(93, 'Choi', 'Hong', 'Gangnam', 'Seoul', '00000', 'Korea');
+
+INSERT INTO Customers
+(CustomerID, CustomerName, ContactName)
+VALUES
+(95, 'BACK', 'Seoul');
+
+INSERT INTO Customers
+(CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+('TRUMP', 'Donald', 'NY', 'NY', '00000', 'US');
+
+-- 테이블 설명 보기
+DESC Customers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
