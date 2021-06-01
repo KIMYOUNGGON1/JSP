b3a5613cac1fe1513d9b79890dfae5113203fff3
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-01 09:47:57 +0900

    20210601 1교시 수업내용

---
+++ WebContent/WEB-INF/sql/UPDATE01.sql
@@ -0,0 +1,55 @@
+SELECT * FROM Customers;
+
+UPDATE Customers
+SET CustomerName = '스파이더맨'
+WHERE CustomerID = 1;
+
+UPDATE Customers
+SET CustomerName = '아이언맨'
+WHERE CustomerID = 1;
+
+UPDATE Customers
+SET CustomerName = 'thor',
+	ContactName = 'odinson',
+    Address = 'Asgard'
+WHERE CustomerID = 1;
+
+
+UPDATE
+ Customers
+SET
+ City = 'Oslo';
+
+
+UPDATE
+ Customers
+SET
+ City = 'seoul'
+WHERE
+ Country = 'Norway';
+
+
+UPDATE
+ Customers
+SET
+ City = 'Oslo',
+ Country = 'Norway'
+WHERE CustomerID = 32;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
