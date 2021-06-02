2f7d845af2d48375a7232301c057e6fea9cd5dd2
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-02 11:47:52 +0900

    20210602 3교시 수업내용

---
+++ WebContent/WEB-INF/sql/CREATE_TABLE03_MEMBER_TYPE.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Practice;
+USE Practice;
+ CREATE TABLE Member 
+ (
+	id VARCHAR(255),
+    password VARCHAR(255),
+    name VARCHAR(255),
+    birth DATE,
+    inserted TIMESTAMP
+ );
