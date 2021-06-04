7b2af0e86f9f06311d59e0efb2737224066c2f3c
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-04 15:08:23 +0900

    20210604 5교시 수업내용

--- src/sample2/controller/board/Sample2BoardDetailServlet.java
+++ src/sample2/controller/board/Sample2BoardDetailServlet.java
@@ -1,28 +1,23 @@
 package sample2.controller.board;
 
 import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import sample2.bean.Board;
-import sample2.dao.BoardDao;
-
 /**
- * Servlet implementation class Sample2BoardListServlet
+ * Servlet implementation class Sample2BoardDetailServlet
  */
-@WebServlet("/sample2/board/list")
-public class Sample2BoardListServlet extends HttpServlet {
+@WebServlet("/sample2/board/detail")
+public class Sample2BoardDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Sample2BoardListServlet() {
+    public Sample2BoardDetailServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,15 +26,8 @@ public class Sample2BoardListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		BoardDao dao = new BoardDao();
-		List<Board> boardList = dao.list();
-		
-		request.setAttribute("boards", boardList);
-		
-		String path = "/WEB-INF/sample2/board/list.jsp";
-		request.getRequestDispatcher(path).forward(request, response); 
-		
-
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
