7c017e2a9351ca6036fe4151320dcd48bcb3da74
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-08 22:25:05 +0900

    20210608 7교시 수업내용

--- WebContent/WEB-INF/sql/TRANSACTION01.sql
+++ WebContent/WEB-INF/sql/TRANSACTION01.sql
@@ -1,9 +1,50 @@
-USE Practice;
+USE test;
 
-SELECT * FROM Member;
-SELECT * FROM Board;
+CREATE TABLE Table05
+(
+name varchar(255) PRIMARY KEY,
+money int NOT NULL DEFAULT 0
+);
 
-UPDATE Board
-SET title = ?,
-	body = ?
-WHERE id = ?
+INSERT INTO Table05 (name, money) VALUES ('kim', 10000);
+INSERT INTO Table05 (name, money) VALUES ('lee', 10000);
+
+SELECT * FROM Table05;
+-- 1
+UPDATE Table05
+SET money = 5000
+WHERE name = 'kim';
+
+-- 2
+SELECT * FROM Table05;
+
+-- 3
+ROLLBACK; -- 원복
+
+-- 4
+SELECT * FROM Table05;
+
+
+-- 동시 실행
+UPDATE Table05
+SET money = 5000
+WHERE name = 'kim';
+
+UPDATE Table05
+SET money = 15000
+WHERE name = 'lee';
+
+COMMIT; -- db 반영
+
+SELECT * FROM Table05;
+
+
+
+
+
+
+
+
+
+
+
--- src/sample2/dao/MemberDao.java
+++ src/sample2/dao/MemberDao.java
@@ -199,12 +199,11 @@ public class MemberDao {
 	}
 
 
-	public void remove(String id) {
+	public void remove(String id, Connection con) {
 		
 		String sql = "DELETE FROM Member WHERE id = ?";
 		
 		try (
-			Connection con = DriverManager.getConnection(url, user, password);
 			PreparedStatement pstmt = con.prepareStatement(sql);
 				) {
 			pstmt.setString(1, id);
